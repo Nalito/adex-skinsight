6077801ac28e894c2e2b21d641a993c8277f563f
Ifeoluwa Oduwaiye <ifeoluwaoduwaiye@gmail.com>
2025-04-25 04:45:16 +0100

    Update ImageDropzone.tsx

--- app/components/ImageDropzone.tsx
+++ app/components/ImageDropzone.tsx
@@ -1,41 +1,21 @@
-"use client";
-import { Icon, loadIcon } from "@iconify/react";
-import React, { useCallback } from "react";
-import { useDropzone } from "react-dropzone";
+import React from "react";
 
-export default function ImageDropzone() {
-  loadIcon("mage:image-upload");
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    // Do something with the files
-  }, []);
-
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    isDragAccept,
-    isDragReject,
-  } = useDropzone({ onDrop });
+export default function ImageDropzone({ onImageUpload }) {
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      onImageUpload(file); // Call the function passed as a prop
+    }
+  };
 
   return (
-    <div
-      {...getRootProps()}
-      className="max-w-[740px] w-full mx-auto mt-10 p-6 border-2 border-neutral text-neutral border-dashed rounded-3xl bg-base-200 transition-colors duration-200 h-96 flex justify-center items-center font-[family-name:var(--font-work-sans)]"
-    >
-      <input {...getInputProps()} />
-
-      <div className="flex flex-col justify-center items-center gap-y-4">
-        <Icon icon="mage:image-upload" className="size-14" />
-        <div className="block text-center space-y-1">
-          <h2 className="font-bold text-base">Drop an image or browse</h2>
-          <p className="text-neutral/70">
-            Format: .jpeg, .png & Max file size: 25 MB
-          </p>
-        </div>
-        <button className="btn btn-neutral w-fit font-normal">
-          Browse images
-        </button>
-      </div>
+    <div className="mt-6 text-center">
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleFileChange}
+        className="file-input"
+      />
     </div>
   );
 }
